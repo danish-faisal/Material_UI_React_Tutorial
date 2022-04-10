f4dad7547812b5da805ac2408304e28c47a548fc
= <danishfaisal.wwe@gmail.com>
2022-04-10 14:30:07 +0530

    Material UI Checkbox

--- src/App.js
+++ src/App.js
@@ -5,11 +5,24 @@ import ButtonGroup from '@mui/material/ButtonGroup';
 import SaveIcon from '@mui/icons-material/Save';
 import SaveAltIcon from '@mui/icons-material/SaveAlt';
 import DeleteIcon from '@mui/icons-material/Delete';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import React from 'react';
+
+function CheckboxExample() {
+  const [checked, setChecked] = React.useState(false);
+  return (
+    <FormControlLabel control={<Checkbox checked={checked} onChange={() => setChecked(!checked)} color="secondary" inputProps={{
+      'aria-label': 'secondary checkbox',
+    }} icon={<DeleteIcon />} checkedIcon={<SaveIcon />} />} label="Checkbox Label" />
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <CheckboxExample />
         <ButtonGroup variant="contained" size="large">
           {/* variant="contained" size="large" href="#" onClick={() => alert("Hello")} disabled  style={{ fontSize: 18 }} endIcon={<SaveAltIcon />}*/}
           <Button startIcon={<SaveIcon />} color="primary">
